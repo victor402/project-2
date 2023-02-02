a0fad0eb7b8d9edc53e43ae33c3218660c9ffd98
victor <victor@victors-MacBook-Air.local>
2023-02-01 22:34:59 -0500

    got the edit function working

--- controllers/reservations.js
+++ controllers/reservations.js
@@ -10,7 +10,44 @@ Restaurant.findById(req.params.id, function(err, restaurant) {
     });
 });
 };
+
+function edit(req, res, next) {
+  Restaurant.findOne({'reservations._id': req.params.id}, function(err, restaurant) {
+console.log(err);
+console.log("successful");
+//console.log(reservation);
+console.log(restaurant.reservations.id(req.params.id))
+const reservation = restaurant.reservations.id(req.params.id)
+console.log(restaurant)
+    res.render('restaurants/edit', {reservation});
+  });
+}
+
+function update(req, res, next) {
+  
+  Restaurant.findOne({'reservations._id': req.params.id}, function(err, restaurant) {
+    
+    const Reservation = restaurant.reservations.id(req.params.id);
+    
+    Reservation.name = req.body.name;
+    Reservation.guests = req.body.guests;
+    Reservation.hours = req.body.hours;
+    Reservation.minutes = req.body.minutes;
+    req.body.time = req.body.hours+ ':' + req.body.minutes;
+    
+    restaurant.save(function(err) {
+      
+      res.redirect(`/restaurants/${restaurant._id}`);
+    });
+  });
+}
+
+
+
+
  
 module.exports = {
-  create
+  create,
+  edit,
+  update,
 };
--- routes/reservations.js
+++ routes/reservations.js
@@ -3,5 +3,7 @@ const express = require('express');
  const reservationsCtrl = require('../controllers/reservations');
  
  router.post('/restaurants/:id/reservations', reservationsCtrl.create);
+ router.get('/reservations/:id/edit', reservationsCtrl.edit);
+ router.put('/reservations/:id', reservationsCtrl.update);
  
  module.exports = router;
